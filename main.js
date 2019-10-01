c7a4e6959cbaed129f22049c6c35e7bf91250267
Daniel Andrade <dsandrade777@gmail.com>
2019-10-01 15:08:21 -0300

    Realizada correcoes no auto upload

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow, ipcMain} = require('electron')
+const {
+  app,
+  BrowserWindow,
+  ipcMain
+} = require('electron');
+const { autoUpdater } = require('electron-updater');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
 function createWindow () {
   // Create the browser window.
@@ -13,19 +18,19 @@ function createWindow () {
     minWidth: 480,
     show: false,
     // icon: path.join(__dirname, 'assets/icons/png/512x512.png')
-  })
+  });
 
   // and load the index.html of the app.
   // mainWindow.loadFile('index.html')
 
-  mainWindow.loadURL('https://admin-sandbox.pedfarma.com.br')
+  mainWindow.loadURL('https://admin-testing.pedfarma.com.br');
 
   mainWindow.once('ready-to-show', () => {
     mainWindow.show()
-  })
+  });
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -33,7 +38,7 @@ function createWindow () {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null
-  })
+  });
 }
 
 // This method will be called when Electron has finished
@@ -41,36 +46,44 @@ function createWindow () {
 // Some APIs can only be used after this event occurs.
 // when the app is loaded create a BrowserWindow and check for updates
 app.on('ready', function() {
-  createWindow()
-  autoUpdater.checkForUpdates();
+  createWindow();
+  autoUpdater.checkForUpdatesAndNotify();
 });
 
 // when the update has been downloaded and is ready to be installed, notify the BrowserWindow
-autoUpdater.on('update-downloaded', (info) => {
-    win.webContents.send('updateReady')
+autoUpdater.on('update-available', () => {
+  mainWindow.webContents.send('update_available');
 });
 
-// when receiving a quitAndInstall signal, quit and install the new version ;)
-ipcMain.on("quitAndInstall", (event, arg) => {
-    autoUpdater.quitAndInstall();
-})
+autoUpdater.on('update-downloaded', () => {
+  mainWindow.webContents.send('update_downloaded');
+});
+
+// when receiving a restart_app signal, quit and install the new version ;)
+ipcMain.on('restart_app', () => {
+  autoUpdater.quitAndInstall();
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', function () {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
+
+ipcMain.on('app_version', (event) => {
+  event.sender.send('app_version', { version: app.getVersion() });
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
